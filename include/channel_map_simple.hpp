69e051ef98b22c78850287a8c2047b3b4d103fe0
SATSUKIUM <koubeoosakahukuoka@gmail.com>
2026-02-19 03:22:02 +0900

    dummy makerの結果をファイル出力するための関数

--- include/channel_map_simple.hpp
+++ include/channel_map_simple.hpp
@@ -33,7 +33,7 @@ namespace chmap {
             void printDETinfo(ChannelMapSimpleItem_DET det_item);
             int getNumberOfChannels() const { return fItems.size(); }
             void makeDummyEntry(uint32_t maxFillFactor = 100); // ソート済みfItems.fe.idの各要素間を最大maxFillFactor個のダミーエントリで等間隔に埋める。
-            void fileoutAllItems(const std::string& output_dir);
+            uint32_t fileoutAllItems(const std::string& output_dir);
 
             ChannelMapSimple(const ChannelMapSimple&) = delete; // prevent copy constructor
             ChannelMapSimple& operator=(const ChannelMapSimple&) = delete; // prevent copy assignment
