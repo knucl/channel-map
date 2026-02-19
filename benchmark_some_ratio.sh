335521d3a0291acecd30fe5b8d91aea43c8a3e31
SATSUKIUM <koubeoosakahukuoka@gmail.com>
2026-02-19 14:46:31 +0900

    fixed ratio benchmark

---
+++ benchmark_some_ratio.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+EXCUTABLE="./build/simple-skeleton-program"
+INPUT_FILE="./mapdata.csv"
+RATIO=(1e-6 1e-5 1e-4 1e-3 1e-2 1e-1)
+for r in "${RATIO[@]}"; do
+    echo "Running with ratio: $r"
+    $EXCUTABLE $INPUT_FILE $r
+done
+echo "Finished all runs."
